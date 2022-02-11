243561fd2ee3e883ff12906d0699ae2833dcf28e
Can Kayalı <cankayali09@gmail.com>
2022-02-11 17:56:49 +0300

    redirect process

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "urun cikisi" */ '../views/UrunCikisi.vue')
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
 ]
 
