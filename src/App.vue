19b80023aa65b91b3237403785b74e15e98f4106
Can Kayalı <cankayali09@gmail.com>
2022-02-14 14:18:28 +0300

    added transition to router-view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <Footer />
   </div>
 </template>
 
-        Navbar
 <script>
 import NavBar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
@@ -29,7 +30,22 @@ body {
   padding: 0;
   box-sizing: border-box;
   background-color: #929292;
-  /* background-color: #b6b4b4; */
   height: 650px;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 0.6s ease-in-out;
+}
+
+.fade-leave {
+}
+
+.fade-leave-active {
+  transition: opacity .7s ease-in-out;
+  opacity: 0;
 }
 </style>
