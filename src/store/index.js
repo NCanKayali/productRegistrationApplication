ece62a8e8bd91f3d5af16622962cc82729b9584c
Can Kayalı <cankayali09@gmail.com>
2022-02-14 13:05:46 +0300

    vuex structure changed

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Products from "./products"
+import Axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    Products
+  },
+
   state: {
-    productsList: [],
+    purchase: 0.0,
+    sale: 0.0,
+    balance: 0.0
+
   },
+
   getters: {
-    purchasePrice(state) {
-      console.log(state.productsList.productPiece)
+    getTradeResult(state) {
+      return {
+        purchase: state.purchase,
+        sale: state.sale,
+        balance: state.balance
+      }
     }
   },
+
   mutations: {
-    setProductsList(state, payload) {
-      state.productsList.unshift(payload)
+    uptadeTradeResult(state, payLoad) {
+      if (payLoad.count) {
+
+        state.purchase += parseFloat(payLoad.purchase) * parseInt(payLoad.count);
+        state.sale += parseFloat(payLoad.sale) * parseInt(payLoad.count);
+
+      }else{
+
+        state.purchase += parseFloat(payLoad.purchase) ;
+        state.sale += parseFloat(payLoad.sale) ;
+
+      }
+
+      state.balance += parseFloat(state.sale) - parseFloat(state.purchase);
+
     }
   },
+
   actions: {
+    setTradeResult({ state, commit }, tradeResult) {
+      commit("uptadeTradeResult", tradeResult);
+
+      let tradeData = {
+        purchase: state.purchase,
+        sale: state.sale
+      }
+      Axios.put("https://urun-ekleme-uygulamasi-default-rtdb.firebaseio.com/products.json", tradeData)
+      // .then(response => {
+      //   console.log(response)
+      // })
+    },
+
+    getTradeResult({ commit }) {
+      Axios.get("https://urun-ekleme-uygulamasi-default-rtdb.firebaseio.com/products.json")
+        .then(response => {
+          commit("uptadeTradeResult",response.data)
+        })
+    }
   },
 })
