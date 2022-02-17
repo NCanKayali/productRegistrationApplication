bbaeabac14625445c3d51edee5f2d8ac5531f2a8
Can Kayalı <cankayali09@gmail.com>
2022-02-17 14:50:53 +0300

    no message

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import {store} from './store'
 
 
 Vue.config.productionTip = false
 
 
 Vue.filter("currency", (value) => {
-  return parseFloat(value).toLocaleString(undefined , {minimumFractionDigits : 2}) + "TL"
+  return parseFloat(value).toLocaleString(undefined , {minimumFractionDigits : 2}) + " TL"
 })
 
 new Vue({
